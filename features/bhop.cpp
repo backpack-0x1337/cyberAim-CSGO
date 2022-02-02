fc8f8bc2933abe78f795990e6b38ec007c782626
kwanl00 <hung.li@uq.net.au>
2022-02-02 22:22:57 +1000

    csgo Internal/External Updated offsets

--- features/bhop.cpp
+++ features/bhop.cpp
@@ -5,9 +5,9 @@
 //
 //
 //int bhop::activate(GameData gd) {
-//    gd->localPlayer.playerEnt = FindDMAAddy(gd->hProcess, gd->moduleBaseAddress + hz::sig::dwLocalPlayer,{0});
+//    gd->localPlayer.playerEnt = FindDMAAddy(gd->hProcess, gd->moduleBaseAddress + hazedumper::signatures::dwLocalPlayer,{0});
 //    ReadProcessMemory(gd->hProcess,
-//                      (BYTE*) (gd->localPlayer.playerEnt + hz::netvars::m_fFlags),
+//                      (BYTE*) (gd->localPlayer.playerEnt + hazedumper::netvars::m_fFlags),
 //                      &gd->localPlayer.flags,
 //                      sizeof(gd->localPlayer.flags),
 //                      NULL);
@@ -15,7 +15,7 @@
 //    if (GetAsyncKeyState(VK_SPACE) && gd->localPlayer.flags & (1 << 0)) {
 //        int x = 6;
 //        WriteProcessMemory(gd->hProcess,
-//                           (BYTE*)gd->moduleBaseAddress + hz::sig::dwForceJump,
+//                           (BYTE*)gd->moduleBaseAddress + hazedumper::signatures::dwForceJump,
 //                           &x,
 //                           sizeof(x),
 //                           NULL);
