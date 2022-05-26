6c9999adb9ac2405e41df2d27dc1dca80eb3ebfd
kwanl00 <hung.li@uq.net.au>
2022-05-26 13:55:50 +1000

    csgo external flash remove hack

--- talk/memory.cpp
+++ talk/memory.cpp
@@ -3,8 +3,8 @@
 
 int main() {
     int health = 100;
-    std:: cout << "Health value is: " << health << std::endl;
-    std:: cout << "Health variable address: " << &health << std::endl;
+    std::cout << "Health value is: " << health << std::endl;
+    std::cout << "Health variable address: " << &health << std::endl;
 
     getc(stdin);// block the code from running
     health -= 100;
